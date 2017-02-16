534ecd5a4f09d381b6b59f13d9416ff7f65c2af7
Simian-Spectre <danecmiller@gmail.com>
2017-02-16 15:31:48 -0800

    win condition and outputs
    Functional outputs for the board (hiding it and updating it). Win
    condition added.

--- Minesweeper/HellSweeper.java
+++ Minesweeper/HellSweeper.java
@@ -5,31 +5,28 @@ import java.util.Scanner;
  */
 public class HellSweeper
 {
+    static int[][] board = new int[5][5];
+    static String[][] visableBoard = new String[5][5];
+    static int safe = 0;
     /**
      * Constructor for objects of class HellSweeper
      */
-    public HellSweeper()
+    public static void main()
     {
-        int[][] board = new int[5][5];
-
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
             {
                 int rng = (int)(Math.random() * 2);
+                if(rng == 0)
+                {
+                    safe += 1;
+                }
                 board[i][j] = rng;
+                visableBoard[i][j] = "x";
             }
         }
-
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-
+        printBoard();
         int playing = 0;
         while(playing == 0)
         {
@@ -44,12 +41,43 @@ public class HellSweeper
             if(board[row][col] == 1)
             {
                 System.out.println("Game over. Welcome to Hellsweeper, the game with literally no strategy or skill.");
+                printRealBoard();
                 playing = 1;
             }
             else
             {
+                safe -= 1;
+                visableBoard[row][col] = "0";
                 System.out.println("You're not dead yet.");
+                printBoard();
+                if(safe <= 0)
+                {
+                    System.out.println("WINNER WINNER MAD LUCK SUPER CHEATS ALERT");
+                    playing = 1;
+                }
             }
+        }
+    }
+    public static void printRealBoard()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printBoard()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                System.out.print(visableBoard[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
