9a22ba369f4920022ec299ea6336749d481a5dd3
Otakar Andrysek <otakar@me.com>
2017-02-19 19:32:17 -0800

    Moved methods above main

--- hellSweeper.java
+++ hellSweeper.java
@@ -17,74 +17,7 @@ public class HellSweeper
     static String[][] visableBoard = new String[board.length - 2][board.length - 2];
     // The number of mines left on the board
     static int safeSpots = 0;
-    
-    /**
-     * This method is the main game run code
-     */
-    public static void main(String[] args)
-    {
-        for (int i = 1; i < board.length - 2; i++)
-        {
-            // Set the i'th row of the first and last column to zero
-            board[i][0] = 0;
-            board[i][board.length - 1] = 0;
-            for (int j = 1; j < board.length - 2; j++)
-            {
-                // Set the j'th column of the first and last row to zero
-                board[0][j] = 0;
-                board[board.length - 1][j] = 0;
-                // This generates a random number 0 | 1, 1 indicates there is a bomb, 0 is safe
-                int isBomb = (int)(Math.random() * 2);
-                if(isBomb == 0)
-                {
-                    // Increment safeSpots, the number of squares without mines
-                    safeSpots += 1;
-                }
-                // Set the board to contain (or not contain) the mines
-                board[i][j] = isBomb;
-                visableBoard[i-1][j-1] = "x";
-            }
-        }
         
-        // The main game loop
-        boolean playing = true;
-        while (playing == true)
-        {
-            // Print the baord to the user
-            printBoard();
-            
-            // Scan for row
-            System.out.println("Enter row number: ");
-            int row = getInput();
-
-            // Scan for column
-            System.out.println("Enter column number: ");
-            int col = getInput();
-        
-            // RIP, the user landed on a mine
-            if (board[row][col] == 1)
-            {
-                System.out.println("Game over. Welcome to Hellsweeper, the game with literally no strategy or skill.");
-                printRealBoard();
-                playing = false;
-            }
-            // Looks like they got lucky
-            else
-            {
-                showNearby(row, col);
-                safeSpots -= 1;
-                visableBoard[row-1][col-1] = Integer.toString(showNearby(row, col));
-                System.out.println("You're not dead yet.");
-                // Maybe there are no more mines left?
-                if(safeSpots <= 0)
-                {
-                    System.out.println("WINNER WINNER MAD LUCK SUPER CHEATS ALERT");
-                    playing = false;
-                }
-            }
-        }
-    }
-    
     /**
      * This method gets user input
      */
@@ -176,5 +109,72 @@ public class HellSweeper
         }
 
         return nearbyMines;
+    }
+    
+    /**
+     * This method is the main game run code
+     */
+    public static void main(String[] args)
+    {
+        for (int i = 1; i < board.length - 2; i++)
+        {
+            // Set the i'th row of the first and last column to zero
+            board[i][0] = 0;
+            board[i][board.length - 1] = 0;
+            for (int j = 1; j < board.length - 2; j++)
+            {
+                // Set the j'th column of the first and last row to zero
+                board[0][j] = 0;
+                board[board.length - 1][j] = 0;
+                // This generates a random number 0 | 1, 1 indicates there is a bomb, 0 is safe
+                int isBomb = (int)(Math.random() * 2);
+                if(isBomb == 0)
+                {
+                    // Increment safeSpots, the number of squares without mines
+                    safeSpots += 1;
+                }
+                // Set the board to contain (or not contain) the mines
+                board[i][j] = isBomb;
+                visableBoard[i-1][j-1] = "x";
+            }
+        }
+        
+        // The main game loop
+        boolean playing = true;
+        while (playing == true)
+        {
+            // Print the baord to the user
+            printBoard();
+            
+            // Scan for row
+            System.out.println("Enter row number: ");
+            int row = getInput();
+
+            // Scan for column
+            System.out.println("Enter column number: ");
+            int col = getInput();
+        
+            // RIP, the user landed on a mine
+            if (board[row][col] == 1)
+            {
+                System.out.println("Game over. Welcome to Hellsweeper, the game with literally no strategy or skill.");
+                printRealBoard();
+                playing = false;
+            }
+            // Looks like they got lucky
+            else
+            {
+                showNearby(row, col);
+                safeSpots -= 1;
+                visableBoard[row-1][col-1] = Integer.toString(showNearby(row, col));
+                System.out.println("You're not dead yet.");
+                // Maybe there are no more mines left?
+                if(safeSpots <= 0)
+                {
+                    System.out.println("WINNER WINNER MAD LUCK SUPER CHEATS ALERT");
+                    playing = false;
+                }
+            }
+        }
     }
 }
