048fa292b95a81246eb1e56a6ffccae88d9157b6
Otakar Andrysek <otakar@me.com>
2017-02-19 18:49:46 -0800

    Added better code comments and improved formatting
    All added a buffer to the board 2-D array

--- hellSweeper.java
+++ hellSweeper.java
@@ -1,34 +1,55 @@
 import java.lang.Math.*;
 import java.util.Scanner;
 /**
- * Minesweeper but every click you die
+ * Title: Minesweeper 
+ * Description: A game where you try to avoid mines.
+ * Authors: Tamir Enkjargle, Dane Miller, Otakar Andrysek
+ * Date: 2/13/17
  */
 public class HellSweeper
 {
-    static int[][] board = new int[5][5];
+    // Initialize board (this is a 5x5 board, two extra rows and column are added for padding)
+    static int[][] board = new int[7][7];
+    // The board that the user sees
     static String[][] visableBoard = new String[5][5];
-    static int safe = 0;
+    
+    // ??
+    static int safeSpots = 0;
     /**
      * Constructor for objects of class HellSweeper
+     * TODO: Move this code out of constructor (it's not supposed to be here I think)
      */
     public static void main(String[] args)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 1; i < board.length - 2; i++)
         {
-            for (int j = 0; j < 5; j++)
+            // Set the i'th row of the first and last column to zero
+            board[i][0] = 0;
+            board[i][board.length - 1] = 0;
+            for (int j = 1; j < board.length - 2; j++)
             {
-                int rng = (int)(Math.random() * 2);
-                if(rng == 0)
+                // Set the j'th column of the first and last row to zero
+                board[0][j] = 0;
+                board[board.length - 1][j] = 0;
+                // This generates a random number 0 | 1, 1 indicates there is a bomb, 0 is safe
+                int isBomb = (int)(Math.random() * 2);
+                if(isBomb == 0)
                 {
-                    safe += 1;
+                    // Increment safeSpots, the number of squares without mines
+                    safeSpots += 1;
                 }
-                board[i][j] = rng;
-                visableBoard[i][j] = "x";
+                // Set the board to contain (or not contain) the mines
+                board[i][j] = isBomb;
+                visableBoard[i-1][j-1] = "x";
             }
         }
+        // Print the board right off the bat
         printBoard();
-        int playing = 0;
-        while(playing == 0)
+        printRealBoard();
+        
+        // The main game loop
+        boolean playing = true;
+        while (playing == true)
         {
             Scanner r = new Scanner(System.in);
             System.out.println("Enter row number: ");
@@ -37,90 +58,91 @@ public class HellSweeper
             Scanner c = new Scanner(System.in);
             System.out.println("Enter column number: ");
             int col = c.nextInt();
-	    
-	    	    
-            if(board[row-1][col-1] == 1)
+        
+            if (board[row][col] == 1)
             {
                 System.out.println("Game over. Welcome to Hellsweeper, the game with literally no strategy or skill.");
                 printRealBoard();
-                playing = 1;
+                playing = false;
             }
             else
             {
-		showNearby(row, col);
-		safe -= 1;
+                showNearby(row, col);
+                safeSpots -= 1;
                 visableBoard[row-1][col-1] = Integer.toString(showNearby(row, col));
                 System.out.println("You're not dead yet.");
                 printBoard();
-                if(safe <= 0)
+                if(safeSpots <= 0)
                 {
                     System.out.println("WINNER WINNER MAD LUCK SUPER CHEATS ALERT");
-                    playing = 1;
+                    playing = false;
                 }
             }
         }
     }
+    
     public static void printRealBoard()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < board.length - 1; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < board.length - 1; j++)
             {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
     }
+    
     public static void printBoard()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < visableBoard.length - 1; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < visableBoard.length - 1; j++)
             {
                 System.out.print(visableBoard[i][j] + " ");
             }
             System.out.println();
         }
     }
+    
     public static int showNearby(int row, int col)
     {
-	int nearbyMines = 0;
-	if (board[row-2][col-2] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row-1][col-2] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row][col-2] == 1)
+        int nearbyMines = 0;
+        if (board[row - 1][col - 1] == 1)
         {
-	    nearbyMines += 1;         
-	}
-	if (board[row-2][col-2] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row][col-1] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row-2][col] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row-1][col] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	if (board[row][col] == 1)
-	{
-	    nearbyMines += 1;
-	}
-	
-	if (nearbyMines == 0) {
-	    return 0;
-	}
-	return 0;
+            nearbyMines += 1;
+        }
+        if (board[row][col - 1] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (board[row][col-2] == 1)
+        {
+            nearbyMines += 1;         
+        }
+        if (board[row-2][col-2] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (board[row][col-1] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (board[row-2][col] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (board[row-1][col] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (board[row][col] == 1)
+        {
+            nearbyMines += 1;
+        }
+        if (nearbyMines == 0) {
+            return 0;
+        }
+        return 0;
     }
 }
